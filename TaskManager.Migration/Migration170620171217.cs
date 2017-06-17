38b756ef4036dcf02d837d5b4101bb9a43ce9fc9
kpoklonski <poklonski.k@gmail.com>
2017-06-17 12:33:45 +0300

    add empty projects for BL and DA, add migration

---
+++ TaskManager.Migration/Migration170620171217.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace TaskManager.Migration
+{
+    [Migration(170620171217)]
+    public class Migration170620171217 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Tasks")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(250).NotNullable()
+                .WithColumn("Description").AsString(int.MaxValue).Nullable()
+                .WithColumn("State").AsInt32().NotNullable()
+                .WithColumn("Priority").AsInt32().NotNullable()
+                .WithColumn("CreatedAt").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentUTCDateTime)
+                .WithColumn("TimeToComplete").AsDateTime().NotNullable();
+        }
+    }
+}
